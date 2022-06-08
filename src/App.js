6416e2c03d64cadb9f516f1aaa3eb8f154caa125
Josh Mallery <josh.mallery@gmail.com>
2022-06-08 08:13:44 -0600

    Add Fetch calls and assocaited logic for same

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ import TriviaContainer from "./components/TriviaContainer";
 import apiCalls from "./apiCalls.js";
 import './App.css';
 
-//hold fetched questions
-//have fetch calls
-//have fetch call logic.
-
-
-
 const App = () => {
+    const [questions, setQuestions] = useState([]);
+    const [error, setError] = useState("");
+    //hold fetched questions - done
+    //have fetch calls - done
+    //have fetch call logic. - done for now
+    //for now just need to take in a number
+  const buildQuery = (queryParams) => {
+    //saving space to later build complex API queries
+    //queryParams should be a number of category at this point
+    fetchQuestions(queryParams)
+  }
+
+
+  const fetchQuestions = (category) => {
+    console.log(category)
+    apiCalls.getQuestions(category)
+      .then(response => setQuestions(response))//set state of questions to response)
+      .catch(error => console.log(error)) //change to error set state??
+  }
+
   return (
     <main className="App">
       <header className="header">
@@ -25,10 +39,11 @@ const App = () => {
           <SideBar />
         </div>
         <div className="routes">
+          { error && <h2></h2> }
           <Switch>
             <Route exact path="/" render={() => <p>Home Page</p>} />
-            <Route path ="/settings" render={() => <Form />} />
-            <Route path ="/trivia" render={() => <TriviaContainer />} />
+            <Route path ="/settings" render={() => <Form buildQuery={buildQuery}/>} />
+            <Route path ="/trivia" render={() => { questions.length === 0 ? <h2>Please go to settings and select your trivia categories first!</h2> : <TriviaContainer questions={questions}/> }} />
             <Route render={() => <h2>No Trivia here! Click Home and Try Again!</h2>} />
           </Switch>
         </div>
