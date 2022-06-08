a3023456d70bfce14b174f7a3d515ee5b1bfcb1d
Josh Mallery <josh.mallery@gmail.com>
2022-06-07 21:13:41 -0600

    add API calls file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Route , Switch } from 'react-router-dom';
+import apiCalls from "./apiCalls.js";
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It's Quizzzzzz Time Y'alllll
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
-    </div>
+    </main>
   );
-}
+};
 
 export default App;
