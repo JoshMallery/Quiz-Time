64903f603df83881e30cf15499eacd1c3026f13d
Josh Mallery <josh.mallery@gmail.com>
2022-06-07 22:07:33 -0600

    Create remaining components for Cards and Scores

---
+++ src/components/Score.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Score.css"
+
+const Score = () => {
+
+//pass down if right or wrong,
+//if right say correct
+//if wrong give correct answer
+//give updated score? score passed as prop
+
+  return (
+    <div className="score">
+      This is the score 3-5.
+    </div>
+  )
+
+}
+
+export default Score;
--- src/components/TriviaContainer.js
+++ src/components/TriviaContainer.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect} from "react";
+import Card from "./Card";
+import Score from "./Score";
+import "./TriviaContainer.css";
 
 const TriviaContainer = () => {
 
+//need way to start the game
   //need prop of questions
   //meed map of question cards
   //need to send up answer and sent to scoring component
 
+
+//scroll down on cards for now? how to cycle through cards on click?
+  //curcard on the mappedCards[1] then mappedCards[2] on the render?
+
+
+//need correct counter
+//need total counter? mappedcards.length?
+
   return (
     <div>
-      trivia content stored here, need a scoring component and a game card
+      <Card />
+      <Score />
+
     </div>
 
   )
