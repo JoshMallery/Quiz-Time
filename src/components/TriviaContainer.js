a5c1ad28c8385338901190d44fe7144aae3435c4
Josh Mallery <josh.mallery@gmail.com>
2022-06-08 21:46:38 -0700

    write logic of game scoring

--- src/components/TriviaContainer.js
+++ src/components/TriviaContainer.js
@@ -4,15 +4,32 @@ import Score from "./Score";
 import "./TriviaContainer.css";
 
 const TriviaContainer = ({questions}) => {
+  const [record, setRecord] = useState({ wins:0 , losses:0 }) //wins and losses
+  const [count, setCount] = useState(0)
   const questionCards = questions.map(question => <Card question={question} />)
 
 
-  //need to send up answer and sent to scoring component
 
+  const calcScore = (boolean, questionNumber) => {
+    if(questionNumber === count){
+      return
+    }
+
+    setCount(count + 1);
+
+    let revisedRecord = {...record};
+
+    boolean ? revisedRecord.wins += 1 : revisedRecord.losses += 1;
+
+    setRecord({...revisedRecord});
+  }
+
+  console.log(calcScore(true,1))
+  console.log(record)
+  //need to send up answer and sent to scoring component
 
 //scroll down on cards for now? how to cycle through cards on click?
   //curcard on the mappedCards[1] then mappedCards[2] on the render?
-
 
 //need correct counter
 //need total counter? mappedcards.length?
@@ -20,7 +37,7 @@ const TriviaContainer = ({questions}) => {
   return (
     <div>
       {questionCards}
-      <Score />
+      <Score record={record}/>
     </div>
 
   )
