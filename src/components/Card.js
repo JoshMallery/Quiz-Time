e356715faaa330150410188c525dc8acd7305e33
Josh Mallery <josh.mallery@gmail.com>
2022-06-09 10:57:23 -0700

    Add radio buttons for quiz

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,27 @@
 import React from "react"
 import "./Card.css"
 
-const Card = ({checkAns,category,correct_answer,incorrect_answers}) => {
+const Card = ({checkAns,category,correct_answer,incorrect_answers,question}) => {
 let randomAns = [...incorrect_answers]
   randomAns.push(correct_answer);
   randomAns.sort((a, b) => 0.5 - Math.random());
 
-//take in props of question, right answer, wrong answers, onclick function to send answer up
-//can I randomize the wrong and right answers?
-//have the question
-//have 4 answers
+//map random ans to radio buttons?
+  const radioAns = randomAns.map((ans,index) => {
+    return (
+      <>
+        <input type="radio" id={index} name="question" value={ans} />
+        {ans}
+      </>)
+    })
+console.log(radioAns)
 //have a button to send the answer back up
 
 
   return (
     <div className="card">
-      I am a trivia question, i will look cool with lots of neat CSS.
+      <p>{question}</p>
+      {radioAns}
     </div>
   )
 
