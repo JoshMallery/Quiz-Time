a7bfb406011fd61a843bfa41f3ee65bab67e5632
Josh Mallery <josh.mallery@gmail.com>
2022-06-10 22:53:03 -0700

    Psuedo Code in the utilities file

--- src/utilities.js
+++ src/utilities.js
@@ -3,6 +3,8 @@ const utilities = {
   cleanText(response) {
 
     const cleanedResponse = response.results.map(question => {
+        return true
+
       //take the question and the correct answer and the incorrect answers and substittute the quptes and apostrophes for the real deal. consider using a regular expression
     })
     return cleanedResponse
