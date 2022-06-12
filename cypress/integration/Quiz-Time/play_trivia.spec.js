d884c4f5576f6c5c610fdc27aa7a27b3ccdf942b
Josh Mallery <josh.mallery@gmail.com>
2022-06-11 23:11:11 -0600

    Complete Initial Testing

--- cypress/integration/Quiz-Time/play_trivia.spec.js
+++ cypress/integration/Quiz-Time/play_trivia.spec.js
@@ -32,16 +32,14 @@ describe('Trivia Page Tests', () => {
       cy.get('button').eq(1).click()
       cy.get('.score').children().contains('Correct Questions:')
       cy.get('.score').children().contains('Total questions: 1')
-
-    })
+    });
 
     it('should update score if question is incorrect', () => {
       cy.get('[type="radio"]').eq(1).check()
       cy.get('button').eq(1).click()
       cy.get('.score').children().contains('Incorrect Questions:')
       cy.get('.score').children().contains('Total questions: 1')
-
-    })
+    });
 
     it('should be able to cycle to the next question and answer it too.', () => {
       cy.get('[type="radio"]').eq(0).check()
@@ -55,22 +53,26 @@ describe('Trivia Page Tests', () => {
       cy.get('.score').children().contains('Incorrect Questions: ')
       cy.get('.score').children().contains('Total questions: 2')
     })
-
-
-
-    it.skip('should have a message if it can not fetch results', () => {
-      cy.intercept("GET",'https://opentdb.com/api.php?amount=5&category=12&difficulty=easy&type=multiple', {
-              statusCode: 500,
-              body: {
-              message: 'Server error. Please try again'
-              }
-
-    })
-
-    it.skip('should have a message if the fetched results haven\'t loaded yet', () => {
-
-
-    })
-
-  })
 })
+
+describe('Trivia Page Server Error Test', () => {
+  beforeEach(() => {
+    cy.intercept("GET",'https://opentdb.com/api.php?amount=5&category=12&difficulty=easy&type=multiple',
+    {
+      statusCode: 500,
+      body: {
+        message: 'Server error. Please try again'
+      }
+    })
+    cy.visit('http://localhost:3000/Quiz-Time/settings')
+    cy.get('select').select(5)
+    cy.get('input').type("{backspace}{backspace}5")
+    cy.get('button').click()
+    cy.get('.nav-button').eq(2).click()
+  });
+
+  it('should have a message if it can not fetch results', () => {
+    cy.get('h2').contains("Server is Down, Please try again later.")
+  });
+
+});
